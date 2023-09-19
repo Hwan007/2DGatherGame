449d1ae543382beee75399fd49d72085abd46697
Hwan007 <ghksdlco@gmail.com>
2023-09-19 12:01:25 +0900

    UI 완성

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +12,50 @@ public class GameManager : MonoBehaviour
 
     public Transform Player { get; private set; }
     [SerializeField] private string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
+
+
 
     private void Awake()
     {
         Instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void UpdateWaveUI()
+    {
+        //waveText.text = 
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        // 현재 실행중인 프로그램에 대해서는 Application에 들어가 있다.
+        Application.Quit();
     }
 }
