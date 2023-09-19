9cba6c4a2e14f044edef486f1173b95b75a5fdb4
Hwan007 <ghksdlco@gmail.com>
2023-09-19 12:34:30 +0900

    게임 로직 구현

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -18,7 +18,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Slider hpGaugeSlider;
     [SerializeField] private GameObject gameOverUI;
 
+    [SerializeField] private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0;
 
+    public float spawnInterval = .5f;
+    public List<GameObject> enemyPrefebs = new List<GameObject>();
+    
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPositions = new List<Transform>();
 
     private void Awake()
     {
@@ -31,11 +40,72 @@ public class GameManager : MonoBehaviour
         playerHealthSystem.OnDeath += GameOver;
 
         gameOverUI.SetActive(false);
+
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPositions.Add(spawnPositionsRoot.GetChild(i));
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartNextWave());
+    }
+
+    
+
+    IEnumerator StartNextWave()
+    {
+        while (true)
+        {
+            if (currentSpawnCount == 0)
+            {
+                UpdateWaveUI();
+                yield return new WaitForSeconds(2f);
+
+                if (currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPositions.Count ? waveSpawnPosCount : waveSpawnPosCount + 1;
+                    waveSpawnCount = 0;
+                }
+
+                if (currentWaveIndex % 5 == 0)
+                {
+                    // reward
+                }
+
+                if (currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+                for (int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPositions.Count);
+                    for (int j = 0; j < waveSpawnCount; j++)
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefebs.Count);
+                        GameObject enemy = Instantiate(enemyPrefebs[prefabIdx], spawnPositions[posIdx].position, Quaternion.identity);
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath;
+                        //enemy.GetComponent<CharacterStatsHandler>()
+                        currentSpawnCount++;
+                        yield return new WaitForSeconds(spawnInterval);
+                    }
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
     }
 
     private void GameOver()
     {
         gameOverUI.SetActive(true);
+        StopAllCoroutines();
     }
 
     private void UpdateHealthUI()
@@ -45,7 +115,7 @@ public class GameManager : MonoBehaviour
 
     private void UpdateWaveUI()
     {
-        //waveText.text = 
+        waveText.text = (currentWaveIndex + 1).ToString();
     }
 
     public void RestartGame()
